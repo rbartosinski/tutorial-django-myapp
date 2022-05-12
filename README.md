dd331956cba68fa5451d9c9cec0e80ebffaac9e8
rbartosinski <r.bartosinski@wp.pl>
2022-05-12 21:05:59 +0200

    Part 2: Django models added

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ pip install django
 
 django-admin startproject mysite
 
+python manage.py startapp polls
+
+python manage.py migrate
+
+python manage.py createsuperuser
+
+python manage.py runserver
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("String %s" % question_id)
+    # return HttpResponse("String {0}".format(question_id))
+
+
+def results(request, question_id):
+    return HttpResponse("String results %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("String vote %s" % question_id)
